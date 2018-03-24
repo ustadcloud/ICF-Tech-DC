0853e0dc6076732dfa9cf43b49f39730a68181df
UstadCloud <ustadcloud@gmail.com>
2018-03-24 05:12:30 +0000

    Docker Install Script

---
+++ install_docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum -y install docker-ce
+
+sudo systemctl start docker
